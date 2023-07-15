1cf9ecee3c7d5189d87f639c437a8a2909760e68
SafiaJotey <safiaahmed1786@gmail.com>
2023-07-15 12:57:26 +0600

    [ADD] initial header footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,3 +1,83 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div className="container bg-[#151b30] p-5">
+        <div className=" text-white flex p-5">
+          <div className="w-4/12 p-5">
+            <h3>BOOK TREKKER</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+              voluptatem natus itaque provident quo dolor magni.
+            </p>
+            <div className="flex justify-between">
+              <h3>facebook</h3>
+              <h3>Twitter</h3>
+              <h3>Instagram</h3>
+              <h3>printerest</h3>
+            </div>
+            <div></div>
+          </div>
+          <div className="w-2/12 p-5">
+            <h3>Important Links</h3>
+            <ul>
+              <li>
+                <a href="">LINK</a>
+              </li>
+              <li>
+                <a href="">LINK</a>
+              </li>
+              <li>
+                <a href="">LINK</a>
+              </li>
+              <li>
+                <a href="">LINK</a>
+              </li>
+              <li>
+                <a href="">LINK</a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-2/12 p-5">
+            <h3>Important Links</h3>
+            <ul>
+              <li>
+                <a href="">LINK</a>
+              </li>
+              <li>
+                <a href="">LINK</a>
+              </li>
+              <li>
+                <a href="">LINK</a>
+              </li>
+              <li>
+                <a href="">LINK</a>
+              </li>
+              <li>
+                <a href="">LINK</a>
+              </li>
+            </ul>
+          </div>
+          <div className="w-4/12 p-5">
+            <h3>BOOK TREKKER</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem
+              voluptatem natus itaque provident quo dolor magni.
+            </p>
+            <div className="flex justify-between">
+              <h3>facebook</h3>
+              <h3>Twitter</h3>
+              <h3>Instagram</h3>
+              <h3>printerest</h3>
+            </div>
+            <div></div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#1e263b] text-white flex justify-center py-5">
+        <p className="py-5">
+          © 2023 BookTrekker By Safia Ahmed. All Rights Reserved
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/layouts/Navbar.tsx
+++ src/layouts/Navbar.tsx
@@ -1,3 +1,50 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
-  return <div>navbar</div>;
+  return (
+    <nav className="w-full h-16 fixed top backdrop-blur-lg z-10 ">
+      <div className="h-full w-full bg-[#003461]">
+        <div className="flex items-center justify-between w-full md:max-w-7xl h-full mx-auto ">
+          <div>
+            <Link className="text-white" to="/home">
+              {' '}
+              Book Trekker
+            </Link>
+          </div>
+          <div>
+            <ul className="flex items-center">
+              <li className=" mx-3">
+                <a>
+                  <Link to="/" className="text-white">
+                    Home
+                  </Link>
+                </a>
+              </li>
+              <li className=" mx-3">
+                <a>
+                  <Link className="text-white" to="/">
+                    All Books
+                  </Link>
+                </a>
+              </li>
+              <li className=" mx-3">
+                <a>
+                  <Link className="text-white" to="/">
+                    Login
+                  </Link>
+                </a>
+              </li>
+              <li className=" mx-3">
+                <a>
+                  <Link className="text-white" to="/">
+                    Sign Up
+                  </Link>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
